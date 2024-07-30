2c67e584caac6a287f1dc8de3ed0cf6f44e7495b
Aayush-1205 <shindeaayush250@gmail.com>
2024-07-31 00:22:17 +0530

    new updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/getBlog",
+        headers: [
+          {
+            key: "Timeout",
+            value: "30s", // increase the timeout to 30 seconds
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
